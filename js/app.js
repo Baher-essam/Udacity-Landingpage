fad62408d4ff52213aac1d7a43bd0edd8bf14a49
Baher-Essam <baher.essam@gmail.com>
2021-05-19 23:22:33 +0200

    adding active links & smooth scrolling

--- js/app.js
+++ js/app.js
@@ -14,18 +14,26 @@
 */
 
 /**
- * Define Global Variables
+ * Define Global letiables
  * 
 */
 const sections = document.querySelectorAll('section');
 const navbar = document.getElementById('navbar__list');
+const navbarList = document.querySelectorAll('.navbar__menu ul li');
 /**
- * End Global Variables
+ * End Global letiables
  * 
  * Start Helper Functions
  * 
 */
-
+function isTopSectionInViewport(el) {
+    // get the coordinates of the element box from the viewport
+    const rect = el.getBoundingClientRect();
+    // return true if the element top is near the top of the viewport
+    return (
+      rect.top >= 0 && rect.top <=  0.4 * (window.innerHeight || document.documentElement.clientHeight)
+    );
+  }
 
 
 /**
@@ -47,22 +55,53 @@ const navBuliding = () => {
 
 }
 navBuliding();
-// Add class 'active' to section when near top of viewport
 
+// Add class 'active' to section when near top of viewport when scrolling
+window.addEventListener('scroll' , () =>{
+  let scrollPosition = document.documentElement.scrollTop;
 
-// Scroll to anchor ID using scrollTO event
+  sections.forEach((section) => {
+    if (
+      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+      scrollPosition <  section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25) {
+      let currentId = section.attributes.id.value;
+      removeAllActiveClasses();
+      addActiveClass(currentId);
+    }
+  });
+})
 
+//remove active class
+let removeAllActiveClasses = function () {
+  document.querySelectorAll("nav a").forEach((el) => {
+    el.classList.remove("active");
+  });
+};
+
+//add active class
+let addActiveClass = function (id) {
+  let selector = `nav ul a[href="#${id}"]`;
+  document.querySelector(selector).classList.add("active");
+};
 
 /**
  * End Main Functions
  * Begin Events
  * 
 */
-
-// Build menu 
-
 // Scroll to section on link click
+let navLinks = document.querySelectorAll("nav ul li a");
 
-// Set sections as active
-
-
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    let currentId = e.target.attributes.href.value;
+    let section = document.querySelector(currentId);
+    let sectionPosition = section.offsetTop;
+//for smooth scrolling when clicking to section links
+    window.scroll({
+      top: sectionPosition,
+      behavior: "smooth",
+    });
+  });
+});
